45d732e3867c2f8cf42ed4585e2294de1982d036
KhanShahab3 <154610064+KhanShahab3@users.noreply.github.com>
2024-11-08 00:24:30 +0500

    setup routing of advertisments,user,wishlist

---
+++ Routes/advertismentsRoutes.js
@@ -0,0 +1,37 @@
+const upload = require("../middlewares/imageMiddleware");
+const Router = require("express").Router();
+const advertisementController = require("../controllers/advertimentsController");
+const bookAdvertisementController = require("../controllers/bookadvertisementController");
+const protect = require("../middlewares/authMiddleware");
+
+Router.post(
+  "/advertisement/create",
+  upload.array("images", 5),
+  advertisementController.addAdvertisement
+);
+Router.get("/advertisement/read", advertisementController.readAdvertisement);
+Router.put(
+  "/advertisement/update/:id",
+  advertisementController.updateAdvertisement
+);
+Router.delete(
+  "/advertisement/delete/:id",
+  advertisementController.deleteAdvertisement
+);
+
+Router.put(
+  "/advertisement/book/:id",
+  bookAdvertisementController.bookAdvertisement
+);
+Router.put("/advertisement/cancel", advertisementController.cancelAdvetisement);
+Router.put(
+  "/advertisement/book/advance/:id",
+  bookAdvertisementController.advanceBooking
+);
+
+Router.get(
+  "/user/bookinghistory/:id",
+  advertisementController.getBookingHistory
+);
+
+module.exports = Router;
