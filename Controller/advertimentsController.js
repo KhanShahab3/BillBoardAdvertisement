2bf827cfcaa84816753cfc23069387e106c59c2e
KhanShahab3 <154610064+KhanShahab3@users.noreply.github.com>
2024-11-08 00:18:44 +0500

    created the userController,BookadvertisementController,wishlistConstroller,advirtismentController

---
+++ Controller/advertimentsController.js
@@ -0,0 +1,129 @@
+const Advertisement = require("../Models/advertismentModel");
+const User = require("../models/usersModel");
+
+const addAdvertisement = async (req, res) => {
+  try {
+    const { title, description, price, location, userId } = req.body;
+
+    const images = req.files.map((file) => ({
+      name: file.originalname,
+      url: `${process.env.PORT_URI}/advertisement/${file.filename}`, // Adjust the URL based on your server configuration
+    }));
+
+    const advertisement = new Advertisement({
+      title,
+      description,
+      pricePerDay: price,
+      images,
+      location,
+      addedBy: userId,
+    });
+
+    await advertisement.save();
+
+    res.status(200).json({ message: "Advertisement created successfully" });
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).json({ error: error.message });
+  }
+};
+const readAdvertisement = async (req, res) => {
+  try {
+    // Find the advertisement by ID
+    const advertisement = await Advertisement.find().populate("bookingId");
+
+    // Check if the advertisement was found
+    if (!advertisement) {
+      return res.status(404).json({ error: "Advertisement not found" });
+    }
+
+    res.status(200).json({ advertisements: advertisement });
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).json({ error: error.message });
+  }
+};
+const updateAdvertisement = async (req, res) => {
+  try {
+    const advertisementId = req.params.id;
+    const { title, description, price, location } = req.body;
+
+    // Find the advertisement by ID
+    const advertisement = await Advertisement.findById(advertisementId);
+
+    // Check if the advertisement was found
+    if (!advertisement) {
+      return res.status(404).json({ error: "Advertisement not found" });
+    }
+    // Update advertisement properties
+    advertisement.title = title;
+    advertisement.description = description;
+    advertisement.pricePerDay = price;
+    advertisement.location = location;
+
+    // Save the changes
+    await advertisement.save();
+
+    res.status(200).json({ message: "Advertisement updated successfully" });
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).json({ error: error.message });
+  }
+};
+const deleteAdvertisement = async (req, res) => {
+  try {
+    const advertisementId = req.params.id;
+
+    // Find and delete the advertisement by ID
+    const result = await Advertisement.findByIdAndDelete(advertisementId);
+
+    // Check if the advertisement was found and deleted
+    if (!result) {
+      return res.status(404).json({ error: "Advertisement not found" });
+    }
+
+    res.status(200).json({ message: "Advertisement deleted successfully" });
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).json({ error: error.message });
+  }
+};
+const cancelAdvetisement = async (req, res) => {
+  try {
+    const advertisementId = req.params.id;
+
+    const advertisement = await Advertisement.findById(advertisementId);
+    // Check if the advertisement is currently booked
+    if (advertisement.status === "booked") {
+      // Reset relevant fields and update the status to 'open for booking'
+      advertisement.status = "open for booking";
+      advertisement.bookedBy = undefined;
+      advertisement.totalBookingDays = undefined;
+      advertisement.reservationStartDate = undefined;
+      advertisement.reservationEndDate = undefined;
+
+      await advertisement.save();
+    }
+  } catch (error) {}
+};
+const getBookingHistory = async (req, res) => {
+  try {
+    const userId = req.params.id;
+
+    // Find the user by ID
+    const user = await User.findById(userId).populate("bookHistory");
+    // Respond with the user's booking history
+    res.status(200).json({ bookingHistory: user.bookHistory });
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).json({ error: error.message });
+  }
+};
+module.exports = {
+  addAdvertisement,
+  readAdvertisement,
+  updateAdvertisement,
+  deleteAdvertisement,
+  cancelAdvetisement,
+  getBookingHistory,
+};
---
+++ Controller/bookadvertisementController.js
@@ -0,0 +1,180 @@
+const schedule = require("node-schedule");
+const Advertisement = require("../Models/advertismentModel");
+const User = require("../models/usersModel");
+const BookingHistory = require("../Models/bookingModel");
+
+const bookAdvertisement = async (req, res) => {
+  try {
+    const advertisementId = req.params.id;
+    const { totalBookingDays, userId } = req.body;
+    // Find the user by ID
+    const user = await User.findOne({ _id: userId });
+    if (!user) {
+      return res.json({ message: "user not found!" });
+    }
+    // Find the advertisement by ID
+    const advertisement = await Advertisement.findById(advertisementId);
+
+    // Check if the advertisement was found
+    if (!advertisement) {
+      return res.status(404).json({ error: "Advertisement not found" });
+    }
+
+    //calculate total price
+    const totalPrice = totalBookingDays * advertisement.pricePerDay;
+
+    // Create a new booking history entry
+    const newBooking = new BookingHistory({
+      userId,
+      advertisementId,
+      totalBookingDays,
+      totalPrice,
+      bookingDate: new Date(),
+      // Other details about the booking
+    });
+
+    // Save the new booking history entry
+    await newBooking.save();
+
+    user.bookHistory.push(newBooking._id);
+    user.save();
+
+    // Update the advertisement properties
+    advertisement.status = "booked";
+    advertisement.bookedBy = userId;
+    advertisement.totalBookingDays = totalBookingDays;
+    // Define reservationStartDate here
+    const reservationStartDate = new Date();
+    advertisement.reservationStartDate = reservationStartDate;
+    // Check if totalBookingDays is a positive integer
+    if (!Number.isInteger(totalBookingDays) || totalBookingDays <= 0) {
+      return res.status(400).json({ error: "Invalid totalBookingDays value" });
+    }
+    // Define and calculate reservationEndDate
+    advertisement.reservationEndDate = new Date(
+      reservationStartDate.getTime() + totalBookingDays * 24 * 60 * 60 * 1000
+    );
+    // pushing id into advertisement collection
+    advertisement.bookingId.push(newBooking._id);
+    // Save the changes
+    await advertisement.save();
+    // Start the background task to automatically cancel reservation
+    const backgroundTaskJob = startBackgroundTask(advertisementId);
+
+    // Respond with success message
+    res.status(200).json({
+      message: "Advertisement Booked Successfully!",
+      totalPrice: totalPrice,
+    });
+  } catch (error) {
+    // Handle errors and respond with an error message
+    console.error(error.message);
+    res.status(500).json({ error: error.message });
+  }
+};
+const advanceBooking = async (req, res) => {
+  try {
+    const advertisementId = req.params.id;
+    const { totalBookingDays, userId } = req.body;
+
+    const user = await User.findById(userId);
+
+    // if user not found
+    if (!user) {
+      return res.json({ message: "user not found!" });
+    }
+    const advertisement = await Advertisement.findById(advertisementId);
+
+    if (!advertisement) {
+      return res.json({ message: "advertisement not found!" });
+    }
+    // Create a new booking history entry with status set to 'pending'
+    const price = advertisement.pricePerDay;
+    const totalPrice = price * totalBookingDays;
+    // Create a new booking history entry with status set to 'pending'
+    const newBooking = await BookingHistory.create({
+      userId,
+      advertisementId,
+      totalBookingDays,
+      totalPrice,
+      bookingDate: new Date(),
+      status: "pending", // Set the initial status to 'pending'
+      // Other details about the booking
+    });
+    // pushing booking id into user booking history
+    user.bookHistory.push(newBooking._id);
+    await user.save();
+
+    // pushing id into advertisement collection
+    advertisement.bookingId.push(newBooking._id);
+    await advertisement.save();
+
+    res.status(200).json({ message: "advance booking done successfully" });
+    // ...
+  } catch (error) {
+    // Handle errors and respond with an error message
+    console.error(error.message);
+    res.status(500).json({ error: error.message });
+  }
+};
+const cancelReservationAndUpdateStatus = async (advertisementId) => {
+  try {
+    const advertisement = await Advertisement.findById(advertisementId);
+
+    // Check if the advertisement is currently booked
+    if (advertisement.status === "booked") {
+      const currentDate = new Date();
+      // Check if the reservationEndDate is in the past
+      if (currentDate > advertisement.reservationEndDate) {
+        // Find the next pending booking
+        const nextBooking = await BookingHistory.findOne({
+          advertisementId,
+          status: "pending",
+        }).sort({ bookingDate: 1 });
+
+        if (nextBooking) {
+          // Assign the advertisement to the next pending booking
+          advertisement.status = "booked";
+          advertisement.bookedBy = nextBooking.userId;
+          advertisement.totalBookingDays = nextBooking.totalBookingDays;
+          advertisement.reservationStartDate = new Date();
+          advertisement.reservationEndDate = new Date(
+            new Date().getTime() +
+              nextBooking.totalBookingDays * 24 * 60 * 60 * 1000
+          );
+
+          // Update the status of the next booking to 'booked'
+          nextBooking.status = "booked";
+          await nextBooking.save();
+        } else {
+          // Reset relevant fields and update the status to 'open for booking'
+          advertisement.status = "open for booking";
+          advertisement.bookedBy = undefined;
+          advertisement.totalBookingDays = undefined;
+          advertisement.reservationStartDate = undefined;
+          advertisement.reservationEndDate = undefined;
+
+          await advertisement.save();
+        }
+      }
+    }
+  } catch (error) {
+    console.error(error.message);
+  }
+};
+const startBackgroundTask = (advertisementId) => {
+  // Set the cron expression for the job (e.g., every hour)
+  const cronExpression = "0 * * * *"; // Runs every hour
+
+  // Schedule the job
+  const job = schedule.scheduleJob(cronExpression, async () => {
+    await cancelReservationAndUpdateStatus(advertisementId);
+  });
+
+  // Optionally, save the job in a data structure if you want to cancel it later
+  return job;
+};
+module.exports = {
+  bookAdvertisement,
+  advanceBooking,
+};
